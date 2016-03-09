fa6f0c28ea621bc28eff065a67ddbf06ebd88e9c
Graeme Sutherland <grasuth@nodestone.io>
2016-03-09 17:34:07 +0000

    eslint and tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,3 +1,20 @@
-QUnit.test('CanvasLife defined', function(assert) {
-    assert.ok(CanvasLife !== undefined);   
+QUnit.module('Basics');
+
+QUnit.test('CanvasLife available', function(assert) {
+  assert.ok(CanvasLife !== undefined);
+});
+
+QUnit.test('resolveObject', function(assert) {
+  var universe = document.getElementById('universe')
+    , life;      
+    
+  life = CanvasLife(universe, {});
+  assert.deepEqual(life.options, life.defaultOptions,
+    'Expect defaults to be used');
+  
+  life = CanvasLife(universe, {a: 'b'});
+  assert.notOk('a' in life.options, 'Option key must be defined in defaults');
+  
+  life = CanvasLife(universe, {aliveValue: 0x1});
+  assert.equal(life.options.aliveValue, 0x1, 'Can override default option');
 });
